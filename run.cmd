39c239e4d5ccfab278dd28f2fd2f73ff61360e07
St-ph <100976300+stephan-hp@users.noreply.github.com>
2023-07-02 18:29:31 -0400

    Update run.cmd
    This fix is for running on any desktop

--- run.cmd
+++ run.cmd
@@ -7,5 +7,16 @@ echo  Daniela AI starting...
 echo  github.com/stephan-hp
 echo.
 cd /d "%~dp0"
+del /s /q .\virtual\pyvenv.cfg
+
+echo home = %USERPROFILE%\AppData\Local\Programs\Python\Python38>>.\virtual\pyvenv.cfg
+echo implementation = CPython>>.\virtual\pyvenv.cfg
+echo version_info = 3.8.0.final.0>>.\virtual\pyvenv.cfg
+echo virtualenv = 20.23.1>>.\virtual\pyvenv.cfg
+echo include-system-site-packages = false>>.\virtual\pyvenv.cfg
+echo base-prefix = %USERPROFILE%\AppData\Local\Programs\Python\Python38>>.\virtual\pyvenv.cfg
+echo base-exec-prefix = %USERPROFILE%\AppData\Local\Programs\Python\Python38>>.\virtual\pyvenv.cfg
+echo base-executable = %USERPROFILE%\AppData\Local\Programs\Python\Python38\python.exe>>.\virtual\pyvenv.cfg
+
 call .\virtual\Scripts\activate.bat
 python daniela.py 
